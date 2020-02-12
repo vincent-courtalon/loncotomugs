58b0a824602a94f6c09b15888c7b73dbba91f00b
Stagiaire <Stagiaire@INSTRUCT.edu-lol.loc>
2020-02-12 11:40:18 +0100

    creation intiale du backend

---
+++ backend/loncoto-mugs-back/src/test/java/com/edugroupe/loncotomugsback/LoncotoMugsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edugroupe.loncotomugsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoncotoMugsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
